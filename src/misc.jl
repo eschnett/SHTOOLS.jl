3e20beb7bf1e6096ba6a135e0f14d5befc45e7db
Jameson Nash <vtjnash@gmail.com>
2023-10-16 11:09:03 -0400

    Update misc.jl

--- src/misc.jl
+++ src/misc.jl
@@ -17,11 +17,11 @@ for all integral values of `j` that satisfy `max(abs(m1), abs(j2 - j3)) <= j <= 
 The values of `jmin` and `jmax` are set to `max(abs(m1), abs(j2 - j3))` and `j2 + j3` respectively.
 The vector `w3j` is filled with the computed 3j symbols.
 Its first index corresponds to `j = jmin`,
-and it has its first `jmax - jmin + 1` indices populated.
+and it has its first `jnum = jmax - jmin + 1` indices populated.
 
 !!! note
-    `w3j` must be sufficiently long to contain all the elements.
-    Pre-existing values in `w3j` may be overwritten.
+    `w3j` must be sufficiently long to contain at least j2+j3+1 elements.
+    Pre-existing values in `w3j[jnum:j2+j3+1]` may be overwritten with zero.
 
 See also: [`Wigner3j`](@ref)
 
